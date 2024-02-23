3b7bc76b340f2849f2431720d370328e72a95415
Jahid458 <jahidul898@outlook.com>
2024-02-24 02:58:36 +0600

    crud commit

---
+++ src/Data.js
@@ -0,0 +1,27 @@
+export const EmployeeData = [
+  {
+    id : 1,
+    firstname: "Nayek",
+    lastName : "Khan",
+    age:32
+  },
+  {
+    id : 2,
+    firstname: "Tania",
+    lastName : "Jaman",
+    age:28
+  },
+  {
+    id : 3,
+    firstname: "A.Kalam",
+    lastName : "Azad",
+    age:40
+  },
+  {
+    id : 4,
+    firstname: "Khalilur",
+    lastName : "Rahman",
+    age:86
+  },
+
+]
